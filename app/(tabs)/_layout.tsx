52b247cc69882a1ef746e84a4c2dd526fd7db459
DavisKoreal <daviskipchirchir19@gmail.com>
2025-02-09 01:24:10 +0300

    new sales page

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Tabs } from 'expo-router'
 import { TabBar } from '@/components/TabBar'
 
+
 const TabLayout = () => {
   return (
     <Tabs tabBar={(props) => <TabBar {...props} />}>
